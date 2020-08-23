4b719dd5772d59ad944e17079c1de8dc09e6bcff
akshat <akshat_g@ch.iitr.ac.in>
2020-08-24 02:25:27 +0530

    Added material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { FormControl ,Select, MenuItem} from '@material-ui/core';
+import "./App.css"
 function App() {
+  const [country,setcountry]=useState("world")
   return (
-    <div>
-      <h1>Covid 19 tracker</h1>
+    <>
+    <div className="app__body">
+    <h1>Covid 19 tracker</h1>
+      <FormControl variant="outlined">
+        <Select value={country}>
+          <MenuItem value="world">World</MenuItem>
+        </Select>
+      </FormControl>
+      
       
     </div>
+    </>
   )
 }
 
